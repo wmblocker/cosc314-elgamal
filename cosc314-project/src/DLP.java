286b05cffe9343cf541129f53595bd4877e50dc2
wmblocker <wmblocker@blockertech.com>
2017-12-04 21:44:25 -0500

    Adjusted DLP to except any input

--- cosc314-project/src/DLP.java
+++ cosc314-project/src/DLP.java
@@ -6,36 +6,42 @@ public class DLP {
 	public int root;
 	public int mod;
 	public int resid;
-	
-	public DLP (){
+
+	public DLP() {
 		insertValues();
-		findPowers();
 	}
-	
-	public void insertValues(){
+
+	public void insertValues() {
 		Scanner input = new Scanner(System.in);
 		this.prime = 0;
 		this.root = 14;
-		this.mod = 9;
+		this.mod = 19;
 	}
-	
-	public void findPowers(){
+
+	public void findPowers() {
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		int i;
-		for(i = 1; i < 7; i++){
-			double result = 0;
-			result = Math.pow(this.root, i);
-			while(result > 19){
-				result = result - 19;
-			}
-			map.put(i, (int)result);
+		int result = 0;
+		int i = 0;
+			
+		while(i < mod) {
+			result = recursiveHelper(1, root, i);
+			
+			map.put(i,(result % 19));
+			i++;
 		}
 		showLogs(map);
 	}
 	
-	public void showLogs(HashMap<Integer, Integer> map){
-		//System.out.println("L();
+	public int recursiveHelper(int n, int root, int count) {
+		n = n * root;
+		if(n > root) {
+			n = n % 19;
+		}
+		return count == 0 ? n : recursiveHelper(n, root, count - 1);
 	}
-	
-	
+
+	public void showLogs(HashMap<Integer, Integer> map) {
+		System.out.println(map.toString());
+	}
+
 }
--- cosc314-project/src/Main.java
+++ cosc314-project/src/Main.java
@@ -1,9 +1,62 @@
-
+import java.util.*;
 public class Main {
 
+	
+	public static int bobNum = 0;
+	public static int aliceNum = 0;
+	
 	public static void main(String[] args) {
 		DLP dlp = new DLP();
+		//dlp.findPowers();
+		int p = 43;
+		int a = 3;
+		int n = 43;
+		int r = 26;
+		int s = 7;
+		int ct = 0;
+		dlp.findPowers();
+		//System.out.println(122500 % 19);
+		//System.out.println(encrypt(p, a, n, r, s));
+		//System.out.println(decrypt(ct, p, a, n, r, s));
+	}
+	
+	public static long encrypt(int p, int a, int n, int r, int s) {
+		int bobKey = 0;
+		int aliceKey = 0;
+		long key = 0L;
+		
+		bobKey = (int)((Math.pow(a, r)) % n);
+		bobNum = bobKey;
+		aliceKey = (int)((Math.pow(a, s)) % n);
+		aliceNum = aliceKey;
+		key = (long)(Math.pow(37, 26)) % 43;
+		
+		return key % 43;
 		
 	}
-
+	
+	public static int decrypt(int ct, int p, int a, int n, int r, int s) {
+		int bobKey = 0;
+		int aliceKey = 0;
+		int key = 0;
+		
+		bobKey = (int)(Math.pow(bobNum, 7)) % n;
+		
+		int count = 0;
+		int num = 0;
+		while(count != 43) {
+			key = (bobKey * count) % 43;
+			if(key == 1) {
+				num = count;
+				count = 43;
+			}
+			else {
+				count++;
+			}
+		}
+		
+		return (num * ct) % 43;
+	}
+	
+	
 }
